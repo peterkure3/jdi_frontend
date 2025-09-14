99ee3bbca79dfb1aa3ac03f9b0370d3f7b57271f
Peter Kure <peterkure256@gmail.com>
2025-09-14 16:39:53 +0300

    Route for modals

---
+++ src/components/shared/modals/index.js
@@ -0,0 +1,7 @@
+// Modal Components Export
+export { default as BaseModal } from './BaseModal';
+export { default as ConfirmationModal } from './ConfirmationModal';
+export { default as FormModal } from './FormModal';
+export { default as ViewModal } from './ViewModal';
+export { default as ExportModal } from './ExportModal';
+export { default as ImportModal } from './ImportModal';
